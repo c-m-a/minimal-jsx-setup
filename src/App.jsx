1a04e82d0591aa699651c23179744157418352a3
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-07-27 13:19:46 -0500

    First commit Minimal Vite + React + Tailwind CSS Setup!

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <h2 className="text-2xl">Minimal Vite + React + Tailwind CSS Setup!</h2>
+    </main>
+  );
+}
+
+export default App;
